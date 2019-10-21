f6cce465599c5d7285c0d6ae7f38b556084e3c4e
Khalid Obaide <sayedkhalidobaide@gmail.com>
2019-10-21 18:43:51 +0800

    There is a problem with files

--- ar/views.py
+++ ar/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello Darsia")
+    context = {
+
+    }
+    return render(request, 'index.html', context)
