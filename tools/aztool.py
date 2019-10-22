53dc6ac09ecd5964aef8815a869df8f6b5056570
Khalid Obaide <sayedkhalidobaide@gmail.com>
2019-10-22 17:32:31 +0800

    Update Aztool

--- tools/aztool.py
+++ tools/aztool.py
@@ -1,6 +1,7 @@
 import hashlib
 import smtplib
 
+
 # md5 function
 def str2md5(my_string):
     m = hashlib.md5()
@@ -20,4 +21,23 @@ def send_mail(owner_email, owner_password, to, sub, msg):
     message = f"Subject: {sub}\n\n{msg}"
     server.sendmail(web, to, message)
     server.quit()
+
+
+def str2arr(array):
+    returnner = []
+    new_array = array
+    new_array = new_array.replace(']', '')
+    new_array = new_array.replace('[', '')
+    while True:
+        is_kame = ',' in new_array
+        if is_kame:
+            kame_place = int(new_array.index(','))
+            pass
+        else:
+            break
+        kame_word = new_array[0: kame_place:]
+        returnner.append(kame_word)
+        kame_word = new_array[0: kame_place + 1:]
+        new_array = new_array.replace(kame_word, '')
+    return returnner
 
